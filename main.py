6f35ebe81bd077e5301b91d74ccb9f3ea8b9e0c2
syota <q3sky0514@gmail.com>
2026-09-12 17:34:49 +0900

    ikado

--- main.py
+++ main.py
@@ -321,8 +321,8 @@ div.stButton > button {
 
 /* 列の左右の余白を減らして、ボタン同士を近づける */
 div[data-testid="column"] {
-    padding-left: -10px;
-    padding-right: -10px;
+    padding-left: -100px;
+    padding-right: -100px;
 }
 </style>
 """, unsafe_allow_html=True)
