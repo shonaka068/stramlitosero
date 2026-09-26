03a60f4e61c537e0a0c10599363ad934733e8c5b
syota <q3sky0514@gmail.com>
2026-09-26 15:09:49 +0900

    ボタンの変更

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import streamlit as st
 st.markdown("""
 <style>
 div.stButton > button {
-    width: 40px;
-    height: 40px;
+    width: 45px;
+    height: 45px;
     padding: 0px;
     margin: 0px;
     border-radius: 0px;
